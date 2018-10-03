15784b652bc4e63d77c13c57dc077686303eda9a
amangit13 <43822821+amangit13@users.noreply.github.com>
2018-10-03 10:35:41 -0700

    simple blink C project
    Blink project

---
+++ M328P/Blink/Blink/main.c
@@ -0,0 +1,26 @@
+/*
+ * Blink.c
+ *
+ * Created: 10/3/2018 10:32:55 AM
+ * Author : home
+ */ 
+
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB = 0xff;
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		PORTB = 0xff;
+		_delay_ms(500);
+		PORTB = 0;
+		_delay_ms(500);
+    }
+}
+
