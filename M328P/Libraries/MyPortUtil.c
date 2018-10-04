d70e30764324ae4c098045a9a6c15ac99ae062f0
amangit13 <43822821+amangit13@users.noreply.github.com>
2018-10-04 12:48:21 -0700

    delete utils

--- M328P/Libraries/MyPortUtil.c
+++
@@ -1,5 +0,0 @@
-#include <avr/io.h>
-
-//#define HPORTB(X) PORTB = PORTB | 1 << X // usage HPORTB(1) will set bit 1 to high. bit is the IO pin that you want to set high or low
-//#define LPORTB(X) PORTB = PORTB & ~(1 <<  X)
-
