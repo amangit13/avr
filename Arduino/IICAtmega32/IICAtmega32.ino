418febb72f73304ea490d78cc781f45f5a7a37cb
amangit13 <43822821+amangit13@users.noreply.github.com>
2018-10-14 23:12:10 -0700

    iic success

--- Arduino/IICAtmega32/IICAtmega32.ino
+++ Arduino/IICAtmega32/IICAtmega32.ino
@@ -1,52 +1,103 @@
 
 //#include <Wire.h>
-#define wait_IIC  while (!(TWCR & (1<< TWINT))) 
+#define twi_wait  while (!(TWCR & (1<< TWINT))) 
+#define twi_start   TWCR |= ((1<<TWEN) | (1<<TWSTA) | (1<<TWINT))
+#define twi_stop     TWCR = ((1<<TWEN)|(1<<TWINT)|(1<<TWSTO))
+#define twi_senddata   TWCR |= ((1<< TWEN) | (1<<TWINT))
+#define twi_status (TWSR & 0xF8)
+#define twi_nack 0x20
+#define twi_setdata(X) TWDR = X
+
+#define twi_ret_on_nack   if (twi_status == twi_nack)\
+  {\
+    twi_stop;\
+    return;  \
+  }
+
+ 
+#define uint8 byte unsigned
+
+void sendCommand(uint8 command)
+{
+  twi_start;
+  twi_wait;
+  twi_setdata = 0x3D;
+  twi_sendata;
+  twi_wait;
+  twi_ret_on_nack
+
+  twi_setdata = 0; // control
+  twi_senddata;
+  twi_ret_on_nack;
+
+  twi_setdata = command;
+  twi_senddata;
+  twi_stop;
+  
+}
+
+
+void oled_begin()
+{
+  DDRC = 0xff;
+  PINC = 0xff;
+  
+  twi_start;
+  twi_wait;
+  twi_setdata = 0x3D;
+  twi_senddata;
+  twi_ret_on_nack;
+  sendCommand(0xae); // display off
+    
+ 
+}
+
+
 
 void setup() {
   // put your setup code here, to run once:
   DDRC = 0xff;
   PINC = 0xff; // enable pullups
-  Serial.begin(9600);
+  Serial.begin(115200);
   
-  // 100Khz. TWBR 72. leave TWSR as is for prescalar of 1. first two bits zero
-  TWBR = 72;
+  // 400Khz. TWBR 12 for 400Khz, or 72 for 100Khz. leave TWSR as is for prescalar of 1. first two bits zero
+  TWBR = 12;
   TWSR &= 0b11111100;
 
-
+  Serial.println("start");
+ 
   // start bit
-  TWCR |= ((1<<TWEN) | (1<<TWSTA) | (1<<TWINT));
-  wait_IIC;
+  twi_start;
+  twi_wait;
 
-  // send dummy address
-  TWDR = 0x3C; //
-  TWCR |= ((1<< TWEN) | (1<<TWINT));
-  wait_IIC;
+  Serial.print("status ");   Serial.println(TWSR);
+  
+  // send address
+  twi_setdata = 0x3D; //
+  twi_senddata;
+  twi_wait;
 
-  if ((TWSR & 0xF8) == 0x20)
+  if (twi_status == 0x20)
   {
     // error
     Serial.println ("nack recieved");
-    // send data
-
     // send stop bit
-    TWCR = ((1<<TWEN)|(1<<TWINT)|(1<<TWSTO));
+    twi_stop;
     Serial.println("stop bit sent");
   }
   else
   {
     Serial.println("ack recieved");
     // send dummy data
-    TWDR = 0; //
-    TWCR |= ((1<<TWEN)|(1<<TWINT));
-    wait_IIC;
+    twi_setdata = 0; //
+    twi_senddata;
+    twi_wait;
 
     // send stop bit
-    TWCR = ((1<<TWEN)|(1<<TWINT)|(1<<TWSTO));
+    twi_stop;
     Serial.println("done dummy date. stop bit sent");
     
   }
-
-  
 }
 
 void loop() {
