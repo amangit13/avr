d20efb17a8244adde19f19b5c7b0f6ec36aaef46
amangit13 <43822821+amangit13@users.noreply.github.com>
2018-10-20 07:41:29 -0700

    cleaning extra code

--- Arduino/SSD1306IIC test/ssd1306_twi/twi.cpp
+++ Arduino/SSD1306IIC test/ssd1306_twi/twi.cpp
@@ -25,8 +25,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "twiport.h"
-//#include <Arduino.h> // for digitalWrite
 #include <hardwareSerial.h>
+
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
 #endif
@@ -35,7 +35,6 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-//#include <pins_arduino.h>
 #include "twi.h"
 
 static volatile uint8_t twi_state;
@@ -50,13 +49,6 @@ static uint8_t twi_masterBuffer[TWI_BUFFER_LENGTH]; // 32
 static volatile uint8_t twi_masterBufferIndex;
 static volatile uint8_t twi_masterBufferLength;
 
-/*static uint8_t twi_txBuffer[TWI_BUFFER_LENGTH];
-static volatile uint8_t twi_txBufferIndex;
-static volatile uint8_t twi_txBufferLength;
-
-static uint8_t twi_rxBuffer[TWI_BUFFER_LENGTH];
-static volatile uint8_t twi_rxBufferIndex;
-*/
 static volatile uint8_t twi_error;
 
 /*
@@ -71,10 +63,6 @@ void twi_init(void)
   twi_state = TWI_READY;
   twi_sendStop = true;		// default value
   twi_inRepStart = false;
-
-  // activate internal pullups for twi.
-  //digitalWrite(SDA, 1);
-  //digitalWrite(SCL, 1);
 
   PORTC = 0xff;
 
@@ -137,97 +125,7 @@ void twi_setFrequency(uint32_t frequency)
     It is 72 for a 16mhz Wiring board with 100kHz TWI */
 }
 
-/*
-   Function twi_readFrom
-   Desc     attempts to become twi bus master and read a
-            series of bytes from a device on the bus
-   Input    address: 7bit i2c device address
-            data: pointer to byte array
-            length: number of bytes to read into array
-            sendStop: Boolean indicating whether to send a stop at the end
-   Output   number of bytes read
-*/
-/*
-  uint8_t twi_readFrom(uint8_t address, uint8_t* data, uint8_t length, uint8_t sendStop)
-  {
-  uint8_t i;
 
-  // ensure data will fit into buffer
-  if(TWI_BUFFER_LENGTH < length){
-   return 0;
-  }
-
-  // wait until twi is ready, become master receiver
-  while(TWI_READY != twi_state){
-   continue;
-  }
-  twi_state = TWI_MRX;
-  twi_sendStop = sendStop;
-  // reset error state (0xFF.. no error occured)
-  twi_error = 0xFF;
-
-  // initialize buffer iteration vars
-  twi_masterBufferIndex = 0;
-  twi_masterBufferLength = length-1;  // This is not intuitive, read on...
-  // On receive, the previously configured ACK/NACK setting is transmitted in
-  // response to the received byte before the interrupt is signalled.
-  // Therefor we must actually set NACK when the _next_ to last byte is
-  // received, causing that NACK to be sent in response to receiving the last
-  // expected byte of data.
-
-  // build sla+w, slave device address + w bit
-  twi_slarw = TW_READ;
-  twi_slarw |= address << 1;
-
-  if (true == twi_inRepStart) {
-   // if we're in the repeated start state, then we've already sent the start,
-   // (@@@ we hope), and the TWI statemachine is just waiting for the address byte.
-   // We need to remove ourselves from the repeated start state before we enable interrupts,
-   // since the ISR is ASYNC, and we could get confused if we hit the ISR before cleaning
-   // up. Also, don't enable the START interrupt. There may be one pending from the
-   // repeated start that we sent ourselves, and that would really confuse things.
-   twi_inRepStart = false;			// remember, we're dealing with an ASYNC ISR
-   do {
-     TWDR = twi_slarw;
-   } while(TWCR & _BV(TWWC));
-   TWCR = _BV(TWINT) | _BV(TWEA) | _BV(TWEN) | _BV(TWIE);	// enable INTs, but not START
-  }
-  else
-   // send start condition
-   TWCR = _BV(TWEN) | _BV(TWIE) | _BV(TWEA) | _BV(TWINT) | _BV(TWSTA);
-
-  // wait for read operation to complete
-  while(TWI_MRX == twi_state){
-   continue;
-  }
-
-  if (twi_masterBufferIndex < length)
-   length = twi_masterBufferIndex;
-
-  // copy twi buffer to data
-  for(i = 0; i < length; ++i){
-   data[i] = twi_masterBuffer[i];
-  }
-
-  return length;
-  }
-*/
-
-/*
-   Function twi_writeTo
-   Desc     attempts to become twi bus master and write a
-            series of bytes to a device on the bus
-   Input    address: 7bit i2c device address
-            data: pointer to byte array
-            length: number of bytes in array
-            wait: boolean indicating to wait for write or not
-            sendStop: boolean indicating whether or not to send a stop at the end
-   Output   0 .. success
-            1 .. length to long for buffer
-            2 .. address send, NACK received
-            3 .. data send, NACK received
-            4 .. other twi error (lost bus arbitration, bus error, ..)
-*/
 uint8_t twi_writeTo(uint8_t address, uint8_t* data, uint8_t length, uint8_t wait, uint8_t sendStop)
 {
   uint8_t i;
@@ -293,40 +191,6 @@ uint8_t twi_writeTo(uint8_t address, uint8_t* data, uint8_t length, uint8_t wait
   else
     return 4;	// other twi error
 }
-
-/*
-   Function twi_transmit
-   Desc     fills slave tx buffer with data
-            must be called in slave tx event callback
-   Input    data: pointer to byte array
-            length: number of bytes in array
-   Output   1 length too long for buffer
-            2 not slave transmitter
-            0 ok
-*/
-/*
-  uint8_t twi_transmit(const uint8_t* data, uint8_t length)
-  {
-  uint8_t i;
-
-  // ensure data will fit into buffer
-  if(TWI_BUFFER_LENGTH < (twi_txBufferLength+length)){
-   return 1;
-  }
-
-  // ensure we are currently a slave transmitter
-  if(TWI_STX != twi_state){
-   return 2;
-  }
-
-  // set length and copy data into tx buffer
-  for(i = 0; i < length; ++i){
-   twi_txBuffer[twi_txBufferLength+i] = data[i];
-  }
-  twi_txBufferLength += length;
-
-  return 0;
-  }*/
 
 /*
    Function twi_attachSlaveRxEvent
