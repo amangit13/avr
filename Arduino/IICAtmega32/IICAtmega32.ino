3ffd74c556fb99547a9bd1b3b7ff0a924ddad617
amangit13 <43822821+amangit13@users.noreply.github.com>
2018-10-29 22:52:31 -0700

    progmem

--- Arduino/IICAtmega32/IICAtmega32.ino
+++ Arduino/IICAtmega32/IICAtmega32.ino
@@ -1,7 +1,7 @@
 #include "SSD1306.h"
 #include "twi.h"
 //#include "SerialDebug.h"
-
+uint8_t const test[] PROGMEM = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff};
 void setup() {
 
   
@@ -9,6 +9,9 @@ void setup() {
   twi_init;
 
   displayBegin();
+  displayData(0,0,1024,1,8);
+  displayPGMData(0,0,7,1,test);
+  
   while(1)
   {
     displayData(32, 2, 96, 0xff, x);
--- Arduino/IICAtmega32/SSD1306.h
+++ Arduino/IICAtmega32/SSD1306.h
@@ -1,3 +1,5 @@
 void displayCommand(uint8_t);
 void displayData(uint8_t,uint8_t,uint16_t, uint8_t, uint8_t);
+void displayPGMData(uint8_t x, uint8_t row, uint8_t xend, uint8_t rowend, uint8_t *adr);
+
 void displayBegin();
