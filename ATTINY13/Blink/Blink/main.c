c6d814b612da9b092c1872caa089c225f281a4ad
amangit13 <43822821+amangit13@users.noreply.github.com>
2020-04-14 22:16:19 -0700

    blink

---
+++ ATTINY13/Blink/Blink/main.c
@@ -0,0 +1,25 @@
+/*
+ * Blink.c
+ *
+ * Created: 4/14/2020 8:59:25 PM
+ * Author : aman
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB = 0xff;
+	PORTB = 0xff;
+    /* Replace with your application code */
+    while (1) 
+    {
+		_delay_ms(200);
+		PORTB = 0x00;
+		_delay_ms(200);
+		PORTB = 0xff;
+		
+    }
+}
+
