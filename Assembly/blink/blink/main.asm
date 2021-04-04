77e4b2321f05d426d0c5d0e9a8b43647b2e82d5b
amangit13 <43822821+amangit13@users.noreply.github.com>
2021-04-04 14:56:17 -0700

    added comments

--- Assembly/blink/blink/main.asm
+++ Assembly/blink/blink/main.asm
@@ -6,8 +6,9 @@
 ;
 .device attiny13a
 
-.include "C:\Files\AVR\Assembly\common\mymacros.inc"
+.include "C:\Files\AVR\Assembly\common\mymacros.inc" ; import macros
 
+;----------------------------------- DATA SEG global variables ------------------------------------
 .DSEG
 	VAR1: .byte 1
 	.macro VAR1_;(register)
@@ -17,16 +18,17 @@
 		read_var @0,VAR1
 	.endm
 
+;----------------------------------- CODE SEG. main program --------------------------------------
 .CSEG
 .ORG 0x0
 	goto REST
 
 .ORG 0x0010
-	.include "C:\Files\AVR\Assembly\common\myutilities.inc"
+	.include "C:\Files\AVR\Assembly\common\myutilities.inc" ; import utilities such as delay functions
 
 REST:
-	R21_ 128
 	set_PB_out (0)
+	R21_ 128
 
 	loop_infinity:	
 		RCALL delay_1_sec
