76cbfc73d5c2c19d217ff7983a80cc2aa9cdff37
amangit13 <43822821+amangit13@users.noreply.github.com>
2021-04-04 10:03:38 -0700

    updataes

--- Assembly/blink/blink/main.asm
+++ Assembly/blink/blink/main.asm
@@ -1,23 +1,42 @@
-.device attiny13a
 ;
 ; blink.asm
 ;
 ; Created: 3/28/2021 8:55:21 AM
 ; Author : amana
 ;
+.device attiny13a
+
 .include "mymacros.inc"
 
+.DSEG
+	VAR1: .byte 1
+	.macro VAR1_;(register)
+		set_var VAR1,@0
+	.endm
+	.macro _VAR1;(register)
+		read_var @0,VAR1
+	.endm
+	.macro VAR1_add;(val)
+		
+	.endm
+.CSEG
 .ORG 0x0
 goto REST
 
 .ORG 0x0010
 .include "myutilities.inc"
 
+
 REST:
-SET_PB_OUT (0)
+R21_ 128
+set_PB_out (0)
 
 loop_infinity:	
 	RCALL delay_1_sec
-	TOGGLE_PB 0
+	toggle_PB (0)
+
+inc R21
+VAR1_ R21
+
 goto loop_infinity
 
