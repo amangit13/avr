56d9dc39e4487e7c93def274094e4e12b5661cff
amangit13 <43822821+amangit13@users.noreply.github.com>
2021-04-12 21:26:12 -0700

    renamed few files

--- Assembly/Atmega-Serial/Atmega-Serial/main.asm
+++ Assembly/Atmega-Serial/Atmega-Serial/main.asm
@@ -6,26 +6,42 @@
 ;
 #define _ATMEGA8_
 .include "C:\Files\AVR\Assembly\common\mymacros_registers.inc"
-.include "C:\Files\AVR\Assembly\common\mymacros.inc"
-
+.include "C:\Files\AVR\Assembly\common\mymacros_jump.inc"
+.include "C:\Files\AVR\Assembly\common\mymacros_memory.inc"
 .dseg
 
 
 .cseg
 .org 0x0000
 	goto RESET
-	reti
-	reti
 
-.include "C:\Files\AVR\Assembly\common\myutilities.inc"
-.include "C:\Files\AVR\Assembly\common\atmegaserial.inc"
+
+.include "C:\Files\AVR\Assembly\common\myproc_utilities.inc"
+.include "C:\Files\AVR\Assembly\common\myproc_atmega_serial.inc"
 goto RESET
+
+message: .DB "HELLO WORLD",0
 
 RESET:
 	init_stack
-
     serial_init_16M_9600
+
+setpointer: PointAtProgConst message
+	
 	LOOP:
-	serial_send_data 65
-	delay_1sec
+		LoadValueOfProgConst_R0
+		
+		if_register_equal_skip_next R0,0 
+			goto continue
+		delay_1sec
+		goto setpointer
+	
+		continue:
+			serial_send_data R0
+			inc ZL
+	
     goto LOOP
+
+
+;--------------- constants --------------
+
