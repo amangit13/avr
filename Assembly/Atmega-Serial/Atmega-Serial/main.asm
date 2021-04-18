688f3a743d0dd1d88aa268d8c8381d0f31561d3a
amangit13 <43822821+amangit13@users.noreply.github.com>
2021-04-17 19:55:15 -0700

    changes

--- Assembly/Atmega-Serial/Atmega-Serial/main.asm
+++ Assembly/Atmega-Serial/Atmega-Serial/main.asm
@@ -10,6 +10,7 @@
 .include "mymacros_memory.inc"
 .dseg
 
+
 .cseg
 .org 000 
 	goto RESET
@@ -22,17 +23,39 @@ message1: .DB "HELLO WORLD. ",0
 
 RESET:
 	init_stack
-    serial_init_16M_9600
-	R20_ 0
-	
+    seria_init_16M_250K
+	R20_ 200
+	R21_ 1 ; positive direction
+
 	LOOP:
-		serial_send_string_cseg message1
-	
-		inc R20
-		R16__ R20
-		serial_send_R16
-		serial_send_data 32
-		delay_1sec
-	
+		;serial_send_string_cseg message1
+		
+		; --------------------- Set direction ----------------------
+		if_reg_equ_goto R20,1,positive
+			else0: 
+				if_reg_equ_goto R20,255,negative
+			else1: 
+				goto cont
+
+		positive:
+			R21_ 1
+			goto cont
+		negative:
+			R21_ 0
+			goto cont
+		;------------------------------------------------------------
+
+
+		;------------------ check direction and send data ----------
+		cont:
+
+		if_reg_equ_goto R21,1,increase
+		else00: dec R20
+			goto send_data
+		increase:
+			inc R20
+		
+		send_Data: serial_send_reg_data R20
+		;delay_1sec
     goto LOOP
 
